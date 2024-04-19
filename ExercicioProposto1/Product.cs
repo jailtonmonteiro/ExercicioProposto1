59d2f72fa4a9c28cbf7e6f46fef92f3b02238b42
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-04-19 19:25:02 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioProposto1/Product.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+
+namespace ExercicioProposto1
+{
+    internal class Product
+    {
+        string name;
+        double price, totalPrice;
+        int quantity;
+
+        public void totalValueStock()
+        {
+            totalPrice = price * quantity;
+            Console.WriteLine($"VALOR TOTAL EM ESTOQUE R$ {totalPrice.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+
+        public void addProducts(int stock)
+        {
+            quantity = quantity + stock;
+        }
+
+        public void removeProducts(int removeStock)
+        {
+            quantity = quantity - removeStock;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+    }
+}
---
+++ ExercicioProposto1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioProposto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+
+            int stock, removeStock;
+
+            Console.Write("NOME DO PRODUTO: ");
+            product.Name = Console.ReadLine();
+
+            Console.Write("PREÇO DO PRODUTO: ");
+            product.Price = double.Parse(Console.ReadLine());
+
+            Console.Write("QUANTIDADE DO PRODUTO: ");
+            product.Quantity = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"\n\n");
+            Console.WriteLine($"PRODUTO: {product.Name}");
+            Console.WriteLine($"PREÇO R$: {product.Price.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"QUANTIDADE: {product.Quantity}");
+
+            Console.WriteLine("\n");
+            Console.Write("DIGITE A QUANTIDADE DE ENTRADA EM ESTOQUE: ");
+            stock = int.Parse(Console.ReadLine());
+
+            product.addProducts(stock);
+
+            Console.WriteLine($"\n\n");
+            Console.WriteLine($"PRODUTO: {product.Name}");
+            Console.WriteLine($"PREÇO R$: {product.Price.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"QUANTIDADE: {product.Quantity}");
+
+            Console.WriteLine("\n");
+            Console.Write("DIGITE A QUANTIDADE DE SAÍDA EM ESTOQUE: ");
+            removeStock = int.Parse(Console.ReadLine());
+
+            product.removeProducts(removeStock);
+
+            Console.WriteLine($"\n\n");
+            Console.WriteLine($"PRODUTO: {product.Name}");
+            Console.WriteLine($"PREÇO R$: {product.Price.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"QUANTIDADE: {product.Quantity}");
+
+            product.totalValueStock();
+        }
+    }
+}
